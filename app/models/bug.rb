3b38e80031d46d8db739be3b1947bedf1d943e14
m-waqas-taj <mohammad.waqas@devsinc.com>
2021-10-05 20:07:22 +0500

    Updation in bug model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -13,16 +13,6 @@ class Bug < ApplicationRecord
   validates :title, :status, :bug_type, presence: true
   validates :title, uniqueness: true
 
-  validate :screenshot_type
-
   enum bug_type: { bug: 0, feature: 1 }
   enum status: { newly: 0, started: 1, resolved: 2 }
-
-  private
-
-  def screenshot_type
-    return if screenshot.attached? && !screenshot.content_type.in?(%('image/png image/gif'))
-
-    errors.add(:screenshot, 'must be a PNG or GIF!')
-  end
 end
